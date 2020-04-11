95d5382a5efb18cbbc9301cbc6e14fae6ebdf848
Dan King <dan.king@vptech.io>
2020-04-10 17:20:24 -0700

    Read port and environment from config file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express')
+var config = require('./config.json') // See config.template.json for sample config file
 var app = express()
-var server = app.listen(3020, function () {
+app.set('port', config.port || 3000)
+app.set('env', config.env || 'development')
+var server = app.listen(app.get('port'), function () {
     console.log('Listening on port %s.', server.address().port)
-    console.log('Website live at http://localhost:3020/')
+    console.log('Website live in ' + config.env +' environment at http://localhost:' + app.get('port') + '/')
     console.log('Press Ctrl-C to terminate.')
 })
