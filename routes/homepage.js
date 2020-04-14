9234e35a35338129d954c45d31c0ed6dbd9397de
Dan King <dan.king@vptech.io>
2020-04-14 15:36:36 -0700

    Read form values and pass back to input page

--- routes/homepage.js
+++ routes/homepage.js
@@ -20,7 +20,19 @@ router.get('/', function(req, res){
 
 // Process form values. Re-display form with selected values and results.
 router.post('/datastore_search', function(req, res){
+    var wso2_gateway_url = req.body.wso2_gateway_url
+    var wso2_ckan_context = req.body.wso2_ckan_context
+    var wso2_subscription_key = req.body.wso2_subscription_key
+    var ckan_resource_id = req.body.ckan_resource_id
+    var ckan_api_key = req.body.ckan_api_key
+    var search_term = req.body.search_term
     res.render('home', {
+        wso2_gateway_url: wso2_gateway_url,
+        wso2_ckan_context: wso2_ckan_context,
+        wso2_subscription_key: wso2_subscription_key,
+        ckan_resource_id: ckan_resource_id,
+        ckan_api_key: ckan_api_key,
+        search_term: search_term
     })
 })
 
