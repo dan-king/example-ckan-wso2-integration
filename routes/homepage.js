99d0a160acb76bdb633a1ec210cc9aa8e162b84e
Dan King <dan.king@vptech.io>
2020-04-13 20:38:54 -0700

    Stub form for WSO2 and CKAN parameters

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var router = express.Router()
 
-// Homepage
+// Show blank form
 router.get('/', function(req, res){
     res.render('home')
+})
+
+// Process form values. Re-display form with selected values and results.
+router.post('/datastore_search', function(req, res){
+    res.render('home', {
+    })
 })
 
 module.exports = router
