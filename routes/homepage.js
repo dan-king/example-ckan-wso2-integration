bd7e30d8eda101c5be1725277140ce338cae4165
Dan King <dan.king@vptech.io>
2020-04-14 16:23:34 -0700

    Construct WSO2 url based on form input

--- routes/homepage.js
+++ routes/homepage.js
@@ -26,13 +26,31 @@ router.post('/datastore_search', function(req, res){
     var ckan_resource_id = req.body.ckan_resource_id
     var ckan_api_key = req.body.ckan_api_key
     var search_term = req.body.search_term
+
+    // ===================================================
+    // Construct url based on input
+    // ===================================================
+    // Strip leading/trailing slashes
+    wso2_gateway_url = wso2_gateway_url.replace(/^\/|\/$/g, '')
+    wso2_ckan_context = wso2_ckan_context.replace(/^\/|\/$/g, '')
+    // Construct URL
+    var wso2_request_url = wso2_gateway_url + '/' + wso2_ckan_context + '/action/datastore_search?id='+ckan_resource_id
+    // Append optional search term
+    if (search_term != '') {
+        wso2_request_url = wso2_request_url + '&q=' + search_term
+    }
+
+    // ===================================================
+    // Invoke API
+    // ===================================================
     res.render('home', {
         wso2_gateway_url: wso2_gateway_url,
         wso2_ckan_context: wso2_ckan_context,
         wso2_subscription_key: wso2_subscription_key,
         ckan_resource_id: ckan_resource_id,
         ckan_api_key: ckan_api_key,
-        search_term: search_term
+        search_term: search_term,
+        wso2_request_url: wso2_request_url
     })
 })
 
